0071ad99045c47f3b726024c85902ec6aa643fef
sara pulido <sara01rizo@gmail.com>
2022-10-15 16:01:02 +0200

    Create: lists controllers 🎛

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,5 +4,35 @@ class ListsController < ApplicationController
     @lists = List.all
   end
 
+  def show
+    @review = Review.new(list: @list)
+  end
 
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @list.destroy
+    redirect_to lists_path
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
 end
