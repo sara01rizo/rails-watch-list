c34432d65c34f5619e9f426573ffdba52cd20428
sara pulido <sara01rizo@gmail.com>
2022-10-19 10:49:19 +0200

    Up: to heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,25 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'json'
+require 'open-uri'
 
-puts "Cleaning up db"
+puts 'Cleaning up db'
 Movie.destroy_all
 
-puts "All cleaned"
+puts 'All cleaned'
 
-5.times do
-  Movie.create(
-    title: Faker::FunnyName.two_word_name
+url = "https://tmdb.lewagon.com/movie/top_rated"
+movie_serialized = URI.open(url).read
+movie_data = JSON.parse(movie_serialized)
+movies = movie_data['results']
+
+20.times do |i|
+  Movie.create!(
+    title: movies[i - 1]['title'],
+    overview: movies[i - 1]['overview'],
+    post_url: "https://image.tmdb.org/t/p/original/#{movies[i - 1]['post_path']}",
+    rating: movies[i - 1]['vote_average']
   )
 end
 
-puts "Done"
+puts 'Done'
